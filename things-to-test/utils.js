09ffa0c271190a1f1ffe2e4b0c183f1ce9e90642
Vincent Audebert <vincent.audebert@yahoo.fr>
2017-02-20 15:11:14 +1300

    100% coverage

--- things-to-test/utils.js
+++ things-to-test/utils.js
@@ -10,22 +10,15 @@ export const getSpecie = (animal) => {
     return animal.specie;
 };
 
-// Returns a function, that, as long as it continues to be invoked, will not
-// be triggered. The function will be called after it stops being called for
-// N milliseconds. If `immediate` is passed, trigger the function on the
-// leading edge, instead of the trailing.
-// FROM Underscore.js
-export const debounce = (func, wait, immediate) => {
+export const debounce = (func, wait) => {
 	let timeout;
 	return function() {
 		const context = this, args = arguments;
 		const later = function() {
 			timeout = null;
-			if (!immediate) func.apply(context, args);
+			func.apply(context, args);
 		};
-		const callNow = immediate && !timeout;
 		clearTimeout(timeout);
 		timeout = setTimeout(later, wait);
-		if (callNow) func.apply(context, args);
 	};
 };
