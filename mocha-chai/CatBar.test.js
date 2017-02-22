92d5c079a8905838256961220af375c3f1907490
Vincent Audebert <vincent.audebert@yahoo.fr>
2017-02-22 15:34:29 +1300

    And I forgot to update mocha test... A very good example of why Jest is much better :)

--- mocha-chai/CatBar.test.js
+++ mocha-chai/CatBar.test.js
@@ -7,7 +7,7 @@ import CatBar from '../things-to-test/CatBar';
 describe('<CatBar />', () => {
     it('it should display the container text', () => {
         const wrapper = shallow(<CatBar />);
-        expect(wrapper.text()).to.contain('I\'m a cat bar. I contain cats.');
+        expect(wrapper.text()).to.contain('I am a cat bar. I contain cats.');
         expect(wrapper.text()).to.not.contain('Meow!');
     });
 
